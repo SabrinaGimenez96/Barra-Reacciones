2b6d840ca7c0ccce406dacd1d9d157826510bebc
SabrinaGimenez96 <119470361+SabrinaGimenez96@users.noreply.github.com>
2025-01-04 15:49:47 -0300

    Update and rename Reacciones.js to Reacciones.jsx

--- src/componentes/Reacciones.jsx
+++ src/componentes/Reacciones.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../estilos/Reacciones.css';
 
-function Reacciones (props) {
+function Reacciones(props) {
   return (
     <div className='contenedor-reacciones'>
-      <img
-        className='emoji-reaccion'
-        src={require(`../imagenes/${props.imagen}.png`)}
-        alt= {props.alt} />
-      <div className='contenedor-nombre-reaccion'>
+      <div>
+        <img
+          className='emoji-reaccion'
+          src={require(`../imagenes/${props.imagen}.png`)}
+          alt={props.alt} />
         <p className='nombre-reaccion'>
           <strong> {props.nombre} </strong>
         </p>
